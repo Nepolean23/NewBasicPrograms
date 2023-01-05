9f99b84aaa46484f7720ce97406c5824eab83583
Nepolean23 <nlean1996@gmail.com>
2023-01-05 16:21:43 +0530

    [Nepolean]-Code for LeapYear Program

---
+++ BasicPrograms/LeapYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class LeapYear
+    {
+        public static void Year()
+        {
+            Console.WriteLine("LeapYear Program");
+
+            Console.WriteLine("Enter a Year in 4digit number to find LeapYear or Not-LeapYear");
+            int year=Convert.ToInt32(Console.ReadLine());
+            if((year%4==0)&&(year%100!=0)||(year%400==0))
+            {
+                Console.WriteLine("Entered Year is LeapYear:" + year);
+            }
+            else
+            {
+                Console.WriteLine("Entered Year is Not LeapYear:" + year);
+            }
+        }
+    }
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -1,11 +1,15 @@
 ﻿using BasicPrograms;
 
-Console.WriteLine("1.FlipcoinProgram");
+Console.WriteLine("1.FlipcoinProgram,2.LeapYear");
 Console.WriteLine("Enter your option to view the program");
 int option=Convert.ToInt32(Console.ReadLine());
 switch(option)
 {
     case 1:
         FlipCoins.Coins();
+        break;
+
+    case 2:
+        LeapYear.Year();
         break;
 }
