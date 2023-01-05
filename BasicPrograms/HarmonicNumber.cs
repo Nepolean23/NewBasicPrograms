09629eb70dea301a2540741f2c9ea88d50126ef8
Nepolean23 <nlean1996@gmail.com>
2023-01-05 17:19:53 +0530

    [Nepolean]-Code for Harmonic Series Program

---
+++ BasicPrograms/HarmonicNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class HarmonicNumber
+    {
+        public static void Harmonicseries()
+        {
+            Console.WriteLine("Harmonic Number Program");
+            Console.WriteLine("Enter the nthterm for the HarmonicSeries");
+            int n=Convert.ToInt32(Console.ReadLine());
+            for(int i=1; i<=n; i++) 
+            {
+                Console.Write("1/" +i+" "+"+ ");
+            }
+
+        }
+    }
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -1,6 +1,6 @@
 ﻿using BasicPrograms;
 
-Console.WriteLine("1.FlipcoinProgram,2.LeapYear,3.PowerOfTwo");
+Console.WriteLine("1.FlipcoinProgram,2.LeapYear,3.PowerOfTwo,4.HarmonicNumber");
 Console.WriteLine("Enter your option to view the program");
 int option=Convert.ToInt32(Console.ReadLine());
 switch(option)
@@ -15,6 +15,9 @@ switch(option)
 
     case 3:
         PowerOfTwo.Squareofnumber(); 
+        break;
+    case 4:
+        HarmonicNumber.Harmonicseries(); 
         break;
 
 }
